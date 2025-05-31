98068d3c8238448cb2629451ccac255c2e7080e1
Miles Whiticker <miles.whiticker@gmail.com>
2025-05-31 16:41:13 +1000

    feat: migrate files to tabs, add random wander behaviour to Mob
    Signed-off-by: Miles Whiticker <miles.whiticker@gmail.com>

--- src/game/GameController.ts
+++ src/game/GameController.ts
@@ -1,52 +1,55 @@
 import Mob from './Mob.ts';
 import { resourceController } from './ResourceController.ts';
+import Vector2 from './Vector2.ts';
 
 class GameController {
-  mobs: Mob[] = [];
-  mainRenderFrameId: number = -1;
-  gameCanvas: HTMLCanvasElement | null = null;
+	mobs: Mob[] = [];
+	mainRenderFrameId: number = -1;
+	gameCanvas: HTMLCanvasElement | null = null;
+	game2dRenderContext: CanvasRenderingContext2D | null = null;
 
-  InitializeGame() {
-    console.log("GameController::InitializeGame() starting...");
+	InitializeGame() {
+	console.log("GameController::InitializeGame() starting...");
 
-    this.gameCanvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
+		this.gameCanvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
+		this.game2dRenderContext = this.gameCanvas.getContext('2d');
 
-    resourceController.LoadImage('./src/assets/redman.png');
-    const newMob = new Mob(200, 200, './src/assets/redman.png');
-    this.mobs.push(newMob);
+		resourceController.LoadImage('./src/assets/redman.png');
+		const newMob = new Mob(new Vector2(300, 300), './src/assets/redman.png');
+		this.mobs.push(newMob);
 
-    this.mainRenderFrameId = requestAnimationFrame(this.Update.bind(this));
+		this.mainRenderFrameId = requestAnimationFrame(this.Update.bind(this));
 
-    console.log("GameController::InitializeGame() finished");
-  }
+		console.log("GameController::InitializeGame() finished");
+	}
 
-  tLastUpdate: number = 0;
-  Update() {
-    //console.log("GameController::Update()",this)
-    const tThisUpdate = Date.now();
-    const deltaTime = tThisUpdate - this.tLastUpdate;
-    this.tLastUpdate = tThisUpdate;
+	tLastUpdate: number = 0;
+	Update() {
+		const tThisUpdate = Date.now();
+		const deltaTime = tThisUpdate - this.tLastUpdate;
+		this.tLastUpdate = tThisUpdate;
+		//console.log("GameController::Update()", deltaTime);
 
-    let ctx: CanvasRenderingContext2D | null = null;
-    if (this.gameCanvas) {
-      ctx = this.gameCanvas.getContext('2d');
-    }
+		if (this.game2dRenderContext && this.gameCanvas) {
+			this.game2dRenderContext.fillStyle = '#ccc'
+			this.game2dRenderContext.fillRect(0, 0, this.gameCanvas.width, this.gameCanvas.height)
+		}
 
-    for (let i = 0; i < this.mobs.length; i++) {
-      const curMob = this.mobs[i];
-      curMob.update(deltaTime);
+		for (let i = 0; i < this.mobs.length; i++) {
+			const curMob = this.mobs[i];
+			curMob.update(deltaTime);
 
-      if (ctx)
-      {
-        const mobImage: HTMLImageElement | null = resourceController.GetImage(curMob.imagepath);
-        if (mobImage) {
-          ctx.drawImage(mobImage, curMob.xpos, curMob.ypos, curMob.xwidth, curMob.ywidth);
-        }
-      }
-    }
+			if (this.game2dRenderContext)
+			{
+				const mobImage: HTMLImageElement | null = resourceController.GetImage(curMob.imagepath);
+				if (mobImage) {
+					this.game2dRenderContext.drawImage(mobImage, curMob.pos.x, curMob.pos.y, curMob.dims.x, curMob.dims.y);
+				}
+			}
+		}
 
-    this.mainRenderFrameId = requestAnimationFrame(this.Update.bind(this));
-  }
+		this.mainRenderFrameId = requestAnimationFrame(this.Update.bind(this));
+	}
 }
 
 export const gameController = new GameController();
--- src/game/Mob.ts
+++ src/game/Mob.ts
@@ -1,23 +1,51 @@
+import Vector2 from './Vector2.ts';
 
 export default class Mob {
-  xpos: number;
-  ypos: number;
-  xwidth: number;
-  ywidth: number;
-  imagepath: string;
-  age: number = 0;
-  isAlive: boolean = false;
+	pos: Vector2;
+	dims: Vector2;
+	targetPos: Vector2 = new Vector2(0, 0);
+	moveSpeed: number = 0.2;
+	imagepath: string;
+	age: number = 0;
+	isAlive: boolean = true;
+	randomWander: boolean = true;
 
-  constructor(startX: number, startY: number, imagepath: string) {
-    this.xpos = startX;
-    this.ypos = startY;
-    this.imagepath = imagepath;
-    this.xwidth = 64;
-    this.ywidth = 64;
-  }
+	constructor(startPos: Vector2, imagepath: string) {
+		this.pos = startPos;
+		this.imagepath = imagepath;
+		this.dims = new Vector2(64, 64);
+	}
 
-  update(deltaTime: number) {
-    if (this.isAlive) {
-      this.age += deltaTime; }
-  }
+	update(deltaTime: number) {
+		if (this.isAlive) {
+			this.age += deltaTime;
+
+			if (this.randomWander){
+
+				//have we reached out target?
+				const distLeftSqrd = this.pos.distSqrd(this.targetPos);
+				const thresholdSqrd = 5**2;
+				const arrived = distLeftSqrd <= thresholdSqrd || distLeftSqrd <= (this.moveSpeed * deltaTime)**2;
+				//console.log(`Mob distLeftSqrd: ${distLeftSqrd}`);
+				if (arrived) {
+					//random new move target
+					this.targetPos.x = Math.random() * 500;
+					this.targetPos.y = Math.random() * 500;
+
+					//console.log(`Arrived! new targetPos:`, this.targetPos);
+				}
+				else
+				{
+					//move towards target
+					const dir = this.targetPos.clone();
+					dir.subtract(this.pos);
+					dir.normalise();
+					dir.multiply(this.moveSpeed * deltaTime);
+					this.pos.add(dir);
+
+					//console.log(`Mob dir:`, dir);
+				}
+			}
+		}
+	}
 }
