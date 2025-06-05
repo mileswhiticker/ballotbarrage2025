f3887721a7574b8fdc14f4f1109b82cf916adfdf
Miles Whiticker <miles.whiticker@gmail.com>
2025-06-05 14:36:27 +1000

    chore: move util .ts files into their own folder, update aliases
    Signed-off-by: Miles Whiticker <miles.whiticker@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
 			'@controllers': path.resolve(__dirname, 'src/game/controllers'),
 			'@components': path.resolve(__dirname, 'src/components'),
 			'@assets': path.resolve(__dirname, 'src/assets'),
-			'@utils': path.resolve(__dirname, 'src/game'),
+			'@utils': path.resolve(__dirname, 'src/utils'),
 		},
 	},
 
