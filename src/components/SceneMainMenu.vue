71102e47d91d9f29ae9b053d9d40b5ff64eb2e0f
Miles <miles.whiticker@gmail.com>
2025-10-17 13:29:17 +1100

    fix: remove redundant arg in onClick handler
    Signed-off-by: Miles <miles.whiticker@gmail.com>

--- src/components/SceneMainMenu.vue
+++ src/components/SceneMainMenu.vue
@@ -3,13 +3,13 @@ import appController, { GAMESCENE } from '@controllers/AppController.ts';
 // import { FwbAlert } from 'flowbite-vue';
 import { FwbButton } from 'flowbite-vue';
 
-function handleClick(event: MouseEvent) {
+function handleClick() {
 	appController.changeScene(GAMESCENE.CHARSELECT);
 }
 </script>
 
 <template>
 	<div class="mb-4 flex justify-center font-bold text-lg p-4">
-		<fwb-button size="lg" @click="handleClick($event)">Begin New Game</fwb-button>
+		<fwb-button size="lg" @click="handleClick()">Begin New Game</fwb-button>
 	</div>
 </template>
